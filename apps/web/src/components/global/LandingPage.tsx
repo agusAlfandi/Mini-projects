bcc6d69376fccbc0e960595d3f9c40a981832b96
Agus <agusalfandi8@gmail.com>
2024-07-05 23:37:57 +0700

    build: adjustment environment frontend and add tailwind css and build navbar with daisyui

---
+++ apps/web/src/components/global/LandingPage.tsx
@@ -0,0 +1,12 @@
+export default function LandingPage() {
+  return (
+    <div className="p-4">
+      <div className="flex justify-center">
+        <h1 className="text-2xl">
+          MANAGE adalah sebuah platform yang menawarkan acara atau event
+          berbayar dan free
+        </h1>
+      </div>
+    </div>
+  );
+}
